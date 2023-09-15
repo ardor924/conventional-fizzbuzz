d85a23f95061ddee5837a2f643907cefe71d9af7
ardor924 <ardor924@gmail.com>
2023-09-15 15:31:56 +0900

    refactor : refactoring code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,17 @@
-def do_fizzbuzz(num:int) :
+def do_fizzbuzz(num:int):
     """
     fizzbuzz 기능을 수행합니다.
     정해진 숫자에 대해,
-    3의 배수는 'fizzbuzz'
+    3의 배수는 'fizz'
     5의 배수는 'buzz'
     15의 배수는 'fizzbuzz'
     나머지는 숫자 그대로를 출력합니다.
     """
-    for i in range(1,num+1) :
-        if i%15 == 0 :
-            print('fizzbuzz')
-        elif i%3 == 0 :
-            print('fizz')
-        elif i % 5 == 0 :
-            print('buzz')    
-        else :
+    for i in range(1,num+1):
+        if i%3==0 or i%5==0:
+            print('fizz'*(i%3==0)+'buzz'%(i%5==0))
+        else:
             print(i)
 
-if __name__ == "__main__" :
-    do_fizzbuzz(16)
-    
+if __name__=='__main__':
+    do_fizzbuzz(16)    
